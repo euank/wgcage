646bd6bb59b5d27d9c799b9f401f7de80bb288c7
Euan Kemp <euank@euank.com>
2025-04-23 03:21:25 +0900

    Trim more code

--- go.mod
+++ go.mod
@@ -5,8 +5,6 @@ go 1.23.1
 require (
 	github.com/alexflint/go-arg v1.5.1
 	github.com/ebitengine/purego v0.8.1
-	github.com/google/gopacket v1.1.19
-	github.com/miekg/dns v1.1.65
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
 	github.com/vishvananda/netlink v1.3.0
 	golang.org/x/sys v0.32.0
@@ -20,11 +18,8 @@ require (
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/vishvananda/netns v0.0.5 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
-	golang.org/x/tools v0.32.0 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
 )
 
--- tcp.go
+++ tcp.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
-	"strings"
 
-	"github.com/google/gopacket/layers"
 	"gvisor.dev/gvisor/pkg/tcpip/adapters/gonet"
 	"gvisor.dev/gvisor/pkg/tcpip/transport/tcp"
 	"gvisor.dev/gvisor/pkg/waiter"
@@ -14,16 +12,6 @@ import (
 type tcpRequest struct {
 	fr *tcp.ForwarderRequest
 	wq *waiter.Queue
-}
-
-func (r *tcpRequest) RemoteAddr() net.Addr {
-	addr := r.fr.ID().RemoteAddress
-	return &net.TCPAddr{IP: addr.AsSlice(), Port: int(r.fr.ID().RemotePort)}
-}
-
-func (r *tcpRequest) LocalAddr() net.Addr {
-	addr := r.fr.ID().LocalAddress
-	return &net.TCPAddr{IP: addr.AsSlice(), Port: int(r.fr.ID().LocalPort)}
 }
 
 func (r *tcpRequest) Accept() (net.Conn, error) {
@@ -40,42 +28,4 @@ func (r *tcpRequest) Accept() (net.Conn, error) {
 	conn := gonet.NewTCPConn(r.wq, ep)
 	r.fr.Complete(false)
 	return conn, nil
-}
-
-func (r *tcpRequest) Reject() {
-	r.fr.Complete(true)
-}
-
-// summarizeTCP summarizes a TCP packet into a single line for logging
-func summarizeTCP(ipv4 *layers.IPv4, tcp *layers.TCP, payload []byte) string {
-	var flags []string
-	if tcp.FIN {
-		flags = append(flags, "FIN")
-	}
-	if tcp.SYN {
-		flags = append(flags, "SYN")
-	}
-	if tcp.RST {
-		flags = append(flags, "RST")
-	}
-	if tcp.ACK {
-		flags = append(flags, "ACK")
-	}
-	if tcp.URG {
-		flags = append(flags, "URG")
-	}
-	if tcp.ECE {
-		flags = append(flags, "ECE")
-	}
-	if tcp.CWR {
-		flags = append(flags, "CWR")
-	}
-	if tcp.NS {
-		flags = append(flags, "NS")
-	}
-	// ignore PSH flag
-
-	flagstr := strings.Join(flags, "+")
-	return fmt.Sprintf("TCP %v:%d => %v:%d %s - Seq %d - Ack %d - Len %d",
-		ipv4.SrcIP, tcp.SrcPort, ipv4.DstIP, tcp.DstPort, flagstr, tcp.Seq, tcp.Ack, len(tcp.Payload))
 }
